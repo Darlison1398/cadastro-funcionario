901fe5998bab7750ebe9feca089a2f5e85db8e0e
Darlison Silva <drsilva123jd@gmail.com>
2026-03-05 14:07:47 -0300

    último Commit 05-02, às 14:08

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA3SYXc-Gj7vpGz5cashOULwM2l92MQ5JM",
-  authDomain: "cadastro-funcionario-6a32d.firebaseapp.com",
-  projectId: "cadastro-funcionario-6a32d",
-  storageBucket: "cadastro-funcionario-6a32d.firebasestorage.app",
-  messagingSenderId: "238698089529",
-  appId: "1:238698089529:web:9ac5d7ad63b1304ebf11e8",
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID",
   measurementId: "G-L534808YHD"
 };
 
